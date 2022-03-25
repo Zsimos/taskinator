3dc1cf8460a5af1fc8b8e4b80a22eee8f13ee3aa
Zander <zandersimos@hotmail.com>
2022-03-25 16:24:25 -0700

    3/25 js updates

---
+++ js/assets/js/game.js
@@ -0,0 +1,14 @@
+
+function fight() { 
+    window.alert("The fight has begun!") ;
+}
+var playerName = window.prompt("What is your robots name");
+console.log(playerName); 
+
+// fight();
+
+console.log("This logs a String, good for leaving yourself a message");
+// this wil do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our robots name is" + playerName);
